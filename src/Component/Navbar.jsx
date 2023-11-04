cc81e39b2f3065e6dad309becefb1d7a8f57ca66
urvashi <urvashikshatriya9@gmail.com>
2023-11-04 11:50:32 +0530

    fourth commit

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,8 +1,55 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
 
 function Navbar() {
+  const [isClothSelected,setIsClothSelected] = useState(false);
+  const [isElectronicSelected,setIsElectronicSelected] = useState(false);
+  const [isJewelrySelected,setIsJewelrySelected] = useState(false);
+  {/*Function for Cloth*/}
+  const handleMouseEnter=()=>{
+    setIsClothSelected(true);
+    console.log("handleMouseEnter")
+};
+const handleMouseLeave=()=>{
+  setIsClothSelected(false);
+}
+const textStyle={
+    backgroundColor: isClothSelected? '#f8f9fa' : 'black',
+    color:isClothSelected?'#007bff':'#e9ecef',
+    cursor : 'pointer',
+}
+
+{/* Function For Electronics*/}
+const handleElectronicMouseEnter=()=>{
+  setIsElectronicSelected(true);
+  console.log("handleMouseEnter")
+};
+
+const handleElectronicMouseLeave=()=>{
+  setIsElectronicSelected(false);
+}
+const electronicTextStyle={
+  backgroundColor: isElectronicSelected? '#f8f9fa' : 'black',
+  color:isElectronicSelected?'#007bff':'#e9ecef',
+  cursor : 'pointer',
+}
+
+{/*Function for Jewelry*/}
+const handleJewelryMouseEnter=()=>{
+  setIsJewelrySelected(true);
+  console.log("handleMouseEnter")
+};
+
+const handleJewelryMouseLeave=()=>{
+  setIsJewelrySelected(false);
+}
+const jewelryTextStyle={
+  backgroundColor: isJewelrySelected? '#f8f9fa' : 'black',
+  color:isJewelrySelected?'#007bff':'#e9ecef',
+  cursor : 'pointer',
+}
+
   return (
     <>
       <div className="bg"></div>
@@ -25,17 +72,17 @@ function Navbar() {
                 style={{ background: 'black', color: 'white', fontSize: '10px' }}
               >
                 <li>
-                  <Link to="/cloths" style={{ fontSize: '20px' }}>
+                  <Link to="/cloths" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} style={{ fontSize: '20px' ,...textStyle}}>
                     Cloth
                   </Link>
                 </li>
                 <li>
-                  <Link to="/electronics" style={{ fontSize: '20px' }}>
+                  <Link to="/electronics" onMouseEnter={handleElectronicMouseEnter} onMouseLeave={handleElectronicMouseLeave} style={{ fontSize: '20px' ,...electronicTextStyle}}>
                     Electronic
                   </Link>
                 </li>
                 <li>
-                  <Link to="/jewelry" style={{ fontSize: '20px' }}>
+                  <Link to="/jewelry" onMouseEnter={handleJewelryMouseEnter} onMouseLeave={handleJewelryMouseLeave} style={{ fontSize: '20px' ,...jewelryTextStyle}}>
                     Jewelry
                   </Link>
                 </li>
